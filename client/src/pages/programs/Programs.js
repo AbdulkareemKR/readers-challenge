a3cba8b732f3fbcbb300ebf1b4b2299dfd70f661
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-03-08 16:41:50 +0300

    netlify

--- client/src/pages/programs/Programs.js
+++ client/src/pages/programs/Programs.js
@@ -18,29 +18,20 @@ function Programs() {
         triggerOnce // to present each element on itself while moving down
         direction="up"
       >
-  <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label
-            >Your Role:
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label
-          >
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form> 
+        <form name="contact" method="post">
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </Fade>
     </div>
   );
